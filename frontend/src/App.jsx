bc5f31a9d29b5d9cccfaf5a58643b59c2f85e524
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-05-15 14:34:49 +0530

    4th part completed - socket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Home from "./pages/home/Home";
 import LogIn from "./pages/login/LogIn";
 import SignUp from "./pages/signup/SignUp";
+import { useAuthContext } from "./context/AuthContext.jsx";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <LogIn />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className="cursor-pointer gap-2 label">
+        <label
+          className={`cursor-pointer gap-2 label ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Male</span>
           <input
             type="checkbox"
             className="checkbox checkbox-info border-slate-700"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
           />
         </label>
       </div>
       <div className="form-control">
-        <label className="cursor-pointer gap-2 label">
+        <label
+          className={`cursor-pointer gap-2 label ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Female</span>
           <input
             type="checkbox"
             className="checkbox checkbox-info border-slate-700"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
           />
         </label>
       </div>
